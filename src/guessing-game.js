f0303ca8502ee4dd993857c1b1b495030bf488f3
Pavel Karenda <korendos@gmail.com>
2019-10-19 21:31:01 +0300

    feat: solved task guessing game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.outsideElement = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.outsideElement = Math.round(this.max - (this.max - this.min)/2);
+        return this.outsideElement;
     }
 
     lower() {
-
+        this.max = this.outsideElement;
     }
 
     greater() {
-
+        this.min = this.outsideElement;
     }
 }
 
